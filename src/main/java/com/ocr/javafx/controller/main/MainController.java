caef4b69fb52cf666c3eeba17e8ff87c6520e0cf
nhtrungg <nhtrung1836@gmail.com>
2026-04-08 20:46:38 +0700

    feat: add barchart controller and structure

--- src/main/java/com/ocr/javafx/controller/main/MainController.java
+++ src/main/java/com/ocr/javafx/controller/main/MainController.java
@@ -3,17 +3,25 @@ package com.ocr.javafx.controller.main;
 import com.ocr.javafx.controller.components.TopbarController;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 public class MainController {
     @FXML
     private AnchorPane sidebar;
 
     @FXML
+    public StackPane barchart;
+
+    @FXML
     private TopbarController topbarController;
+
+    @FXML
+    private BarchartController barchartController;
 
     @FXML
     public void initialize() {
         topbarController.setMainController(this);
+        barchartController.setupBarchart();
     }
 
     @FXML
