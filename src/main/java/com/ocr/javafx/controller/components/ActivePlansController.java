8bab99f5825d1c8df4dbfd178d0e1a4eec20e77f
nhtrungg <nhtrung1836@gmail.com>
2026-04-16 22:16:42 +0700

    feat: add active plans section structure

---
+++ src/main/java/com/ocr/javafx/controller/components/ActivePlansController.java
@@ -0,0 +1,7 @@
+package com.ocr.javafx.controller.components;
+
+import javafx.scene.layout.VBox;
+
+public class ActivePlansController {
+    public VBox plansContainer;
+}
---
+++ src/main/java/com/ocr/javafx/controller/components/PlanCardController.java
@@ -0,0 +1,4 @@
+package com.ocr.javafx.controller.components;
+
+public class PlanCardController {
+}
